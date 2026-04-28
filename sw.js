77d4343834e4066534d2b28b3be64b119f0bbd31
wrmp69 <kevin-inter@live.fr>
2026-04-28 12:24:25 +0200

    Update CACHE_NAME to gymlog-v14
    ajoute du mode suivi

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
 //             Network First pour données externes
 // ═══════════════════════════════════════════════
 
-var CACHE_NAME = 'gymlog-v13';
+var CACHE_NAME = 'gymlog-v14';
 var OFFLINE_URL = 'index.html'; 
 
 // Assets à mettre en cache immédiatement
