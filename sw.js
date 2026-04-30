6cba4d648aa80b4d2825b09a54caf30c62940be6
wrmp69 <kevin-inter@live.fr>
2026-05-01 00:16:10 +0200

    Update CACHE_NAME to version 10

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
 //             Network First pour données externes
 // ═══════════════════════════════════════════════
 
-var CACHE_NAME = 'pecday-v9';
+var CACHE_NAME = 'pecday-v10';
 var OFFLINE_URL = 'index.html'; 
 
 // Assets à mettre en cache immédiatement
