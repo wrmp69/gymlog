7e3b3371a54701fa56913b24897c841a00206eab
wrmp69 <kevin-inter@live.fr>
2026-05-02 18:33:03 +0200

    Update cache name from v11 to v12

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
 //             Network First pour données externes
 // ═══════════════════════════════════════════════
 
-var CACHE_NAME = 'pecday-v11';
+var CACHE_NAME = 'pecday-v12';
 var OFFLINE_URL = 'index.html'; 
 
 // Assets à mettre en cache immédiatement
