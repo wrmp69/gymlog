87c5e4a662b8a77e2d58f37be0b3242552cca461
wrmp69 <kevin-inter@live.fr>
2026-05-03 00:01:43 +0200

    Change CACHE_NAME to 'pecday-v13'
    Updated cache name to version 13 for service worker.

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
 //             Network First pour données externes
 // ═══════════════════════════════════════════════
 
-var CACHE_NAME = 'pecday-v12';
+var CACHE_NAME = 'pecday-v13';
 var OFFLINE_URL = 'index.html'; 
 
 // Assets à mettre en cache immédiatement
